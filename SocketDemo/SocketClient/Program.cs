1681029d149434a52c469c68694b33d084def12b
jreey <qdw8911@sina.com>
2014-11-27 15:14:08 +0800

    add socket demo

---
+++ SocketDemo/SocketClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] receiveBytes = new byte[1024];
+            //解析域名或者IP
+            IPHostEntry ipHost = Dns.GetHostEntry("127.0.0.1");
+            //获取与主机关联的IP地址列表
+            IPAddress ipAddress = ipHost.AddressList[3];
+            //IP地址和端口号连接为一个节点
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 8008);
+            Console.WriteLine("Starting： 创建 Socket 对象");
+
+            //创建发送socket实例
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //建立远程主机连接
+            sender.Connect(ipEndPoint);
+            Console.WriteLine("成功连接到：{0}", sender.RemoteEndPoint);
+
+            string sendingMessage = "Hello my first socket test";
+            Console.WriteLine("信息:{0}", sendingMessage);
+            byte[] forwardMessage = Encoding.ASCII.GetBytes(sendingMessage + "[FINAL]");
+            // 将数据发送到连接的Socket
+            sender.Send(forwardMessage);
+            //接受数据，写入缓冲区
+            int totalBytesRecevied = sender.Receive(receiveBytes);
+            Console.WriteLine("准备从服务端接受信息：{0}", Encoding.ASCII.GetString(receiveBytes, 0, totalBytesRecevied));
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SocketDemo/SocketService/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting：  创建 Socket 对象");
+            //创建实例
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //绑定端口
+            listener.Bind(new IPEndPoint(IPAddress.Any, 8008));
+            //侦听端口
+            listener.Listen(10);
+
+            while (true)
+            {
+                Console.WriteLine("正在打开 8008 端口");
+                //新建接受实例
+                Socket socket = listener.Accept();
+                string receiveValue = string.Empty;
+                while (true)
+                {
+                    byte[] receiveBytes = new byte[1024];
+                    //接收数据
+                    int numBytes = socket.Receive(receiveBytes);
+                    Console.WriteLine("开始接受 .");
+                    receiveValue += Encoding.ASCII.GetString(receiveBytes, 0, numBytes);
+                    if (receiveValue.IndexOf("[FINAL]") > -1)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("服务端接受的信息：{0}", receiveValue);
+                string replyValue = "Message successfully recevied";
+                byte[] replyMessage = Encoding.ASCII.GetBytes(replyValue);
+                //将数据发送到连接的Socket
+                socket.Send(replyMessage);
+                //禁用接受和发送
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+        }
+    }
+}
